76c087760347a9ac3dd03c56fac331bfee44aeda
adaefsaf <99067320+adaefsaf@users.noreply.github.com>
2025-03-30 08:05:35 +0700

    anth push

--- Projects/script.js
+++ Projects/script.js
@@ -18,12 +18,3 @@ document.addEventListener('DOMContentLoaded', () => {
         lastScrollY = window.scrollY; // Perbarui posisi scroll terakhir
     });
 });
-
-if (window.location.pathname === '/') {
-    window.location.replace('/home')
-}
-
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-}
